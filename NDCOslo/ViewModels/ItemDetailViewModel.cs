bb3b75439bc65c251be3adbb3cd5b5bddaedfc8b
Clifford Agius <CliffordAgius@CA-Software.co.uk>
2020-06-10 22:29:44 +0100

    First Commit

---
+++ NDCOslo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NDCOslo.Models;
+
+namespace NDCOslo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ NDCOslo/ViewModels/NDCOsloViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace NDCOslo.ViewModels
+{
+    public class NDCOsloViewModel
+    {
+        public ICommand NDCBUttonCommand { get; set; }
+
+        public NDCOsloViewModel()
+        {
+            NDCBUttonCommand = new Command(ActionButton);
+        }
+
+        private async void ActionButton()
+        {
+            try
+            {
+                var Address = "Oslo Spektrum Arena 0185 Oslo Norway";
+                var Locations = await Geocoding.GetLocationsAsync(Address);
+
+                var location = Locations?.FirstOrDefault();
+
+                if (location != null)
+                {
+                    var options = new MapLaunchOptions { NavigationMode = NavigationMode.Driving };
+                    await Map.OpenAsync(location, options);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error {ex.Message}");
+            }
+        }
+    }
+}
